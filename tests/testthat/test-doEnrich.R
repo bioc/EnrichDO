e468c771a97788724c82e9b8a39260324589a040
CHENGliang!@#123 <liangcheng@hrbmu.edu.cn>
2024-08-26 14:17:18 +0800

    modification

--- tests/testthat/test-doEnrich.R
+++ tests/testthat/test-doEnrich.R
@@ -2,6 +2,10 @@ library(EnrichDO)
 
 test_that("doEnrich", {
   demo.data=c(1636,351,102,2932,3077,348,4137,54209,5663,5328,23621,3416,3553)
-  res <- sapply(c("fisherTest","hypergeomTest","binomTest","chisqTest","logoddTest" ), function(test){ aa<-doEnrich(interestGenes = demo.data, test = test) ;return(aa@enrich$p)})
-  expect_true(all(res >= 0) && all(res <= 1))
+  res <- sapply(1:5, function(m){
+    aa<-doEnrich(interestGenes=demo.data, m=m) ;
+    result<-EnrichTab(object=aa,all=TRUE);
+    bb<-filter(result,level<m)$p;
+    return(all(as.numeric(bb) == 1) )})
+  expect_true(all(res) == TRUE)
 })
---
+++ tests/testthat/test-drawGraphViz.R
@@ -0,0 +1,21 @@
+library(EnrichDO)
+
+test_that("drawGraphViz", {
+  demo.data=c(1636,351,102,2932,3077,348,4137,54209,5663,5328,23621,3416,3553)
+  res <- sapply(c(0.01,0.02,0.03,0.04), function(delta){
+
+    aa<-doEnrich(interestGenes=demo.data, delta=delta) ;
+    result<-EnrichTab(object=aa,all=FALSE);
+    record<-1
+    record<-tryCatch({
+      drawGraphViz(enrich = result,n = 5)
+      1
+    },error = function(e){
+      2
+    },finally = {
+      1
+    })
+    return(record)
+    })
+  expect_true(all(res) == 1)
+})
